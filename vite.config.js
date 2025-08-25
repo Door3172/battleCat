edc7d17d3094269eb305a8596170f418318ee30f
Door3172 <149035122+Door3172@users.noreply.github.com>
2025-08-25 22:24:46 +0800

    setup GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// GitHub Pages 要用子路徑：/<repo名>/
+// 你的 repo 是 battleCat，所以 base = '/battleCat/'
 export default defineConfig({
+  base: '/battleCat/',
   plugins: [react()],
-});
+})
